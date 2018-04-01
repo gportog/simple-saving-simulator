77e94c8806f2077676cdeae6c214872301980c08
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2018-04-01 11:09:28 -0300

    Add calcs module :art:
    Functions called by the menus.sh
    - option_A: expected amount by month in an expected period of time
    - option_B: unexpected amount by month in an unexpected period of
      time
    
    Signed-off-by: Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>

--- app/bank_simulator.sh
+++ app/bank_simulator.sh
@@ -17,70 +17,8 @@
 #                                                                             #
 ###############################################################################
 
-source "handlers.sh"
-source "menus.sh"
-
-# ========================= APP ========================== #
-option_A() {
-
-echo ''
-
-read -p 'Quantidade inicial na poupança[R$]: ' amount
-check_decimal_input $amount option_A
-
-read -p 'Rendimento mensal[%]: ' interest_rate
-check_interest_rate_input $interest_rate option_A
-
-read -p 'Depósitos mensais[R$]: ' month_amount
-check_decimal_input $amount option_A
-
-read -p 'Período aplicado[meses]: ' month
-check_month_input $month option_A
-
-for ((i=1; i<=$month; i++))
-do
-profitability=`bc<<EOF
-scale=5
-$amount * $interest_rate
-EOF`
- 
-amount=`bc<<EOF
-scale=5
-$amount + $profitability + $month_amount
-EOF`
-done
-
-echo $amount
-
-}
-
-option_B() {
-
-echo ''
-
-if [ -z $amount ] || [ -z $interest_rate ]
-then 
- read -p 'Quantidade inicial na poupança[R$]: ' amount
- check_decimal_input $amount option_B
- read -p 'Rendimento mensal[%]: ' interest_rate
- check_interest_rate_input $interest_rate option_B
-fi
-
-amount_month option_B
-
-profitability=`bc<<EOF
-scale=5
-$amount * $interest_rate
-EOF`
- 
-amount=`bc<<EOF
-scale=5
-$amount + $profitability + $month_amount
-EOF`
-
-echo "Você terá disponível no próximo mês R$ $amount"
-continue_function option_B
-
-}
+source "handlers.sh" # secondary functions
+source "menus.sh" # main menu and the script info
+source "calcs.sh" # functions called by the main menu
 
 menu
