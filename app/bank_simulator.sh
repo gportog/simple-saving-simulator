ca347d58b9e2bcbfec1ff93f6eaf7d6f73a5adf8
Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>
2018-04-01 12:02:54 -0300

    Organize the modules into handlers folder :art:
    - The app functions now are called in the handlers folder
      by the bank_simulator.sh.
    - Add verification to see if all the scripts into handlers
      folder are ready to be used.
    
    Signed-off-by: Gustavo Porto Guedes <gustavo.guedes@fatec.sp.gov.br>

--- app/bank_simulator.sh
+++ app/bank_simulator.sh
@@ -17,8 +17,27 @@
 #                                                                             #
 ###############################################################################
 
-source "handlers.sh" # secondary functions
-source "menus.sh" # main menu and the script info
-source "calcs.sh" # functions called by the main menu
+txtund=$(tput sgr 0 1)          # Underline
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) #  red
+bldylw=${txtbld}$(tput setaf 3) #  yellow
+bldgrn=${txtbld}$(tput setaf 2) #  green
+txtrst=$(tput sgr0)             # Reset
+INFO=${bldgrn}INFO:${txtrst}
+ERROR=${bldred}ERROR:${txtrst}
+WARN=${bldylw}WARNING:${txtrst}
+MESSAGE="Check if this file exists, if it is not 
+empty or with executable rights."
+
+if [ ! -x "handlers/handlers.sh" ] || [ ! -s handlers/handlers.sh ]; 
+then echo "$ERROR handlers/handlers.sh. $MESSAGE"; exit -1; fi
+if [ ! -x "handlers/menus.sh" ] || [ ! -s handlers/menus.sh ];
+then echo "$ERROR handlers/menus.sh. $MESSAGE";  exit -1; fi 
+if [ ! -x "handlers/calcs.sh" ] || [ ! -s handlers/calcs.sh ];
+then echo "$ERROR handlers/calcs.sh. $MESSAGE";  exit -1; fi                            
+
+source "handlers/handlers.sh" # secondary functions
+source "handlers/menus.sh" # main menu and the script info
+source "handlers/calcs.sh" # functions called by the main menu
 
 menu
